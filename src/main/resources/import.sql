1d6d669010071ff0e59f0c299f6237128be67665
alefo28 <alefo28@gmail.com>
2023-09-19 11:24:51 -0600

    Tarea 1

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO computadora (name, marca, created_at) VALUES ("Pro", "macbook", now());
+INSERT INTO computadora (name, marca, created_at) VALUES ("Desk", "Generic", now());
+INSERT INTO computadora (name, marca, created_at) VALUES ("Yoga", "Lenovo", now());
+INSERT INTO computadora (name, marca, created_at) VALUES ("T6", "Alienware", now());
+INSERT INTO computadora (name, marca, created_at) VALUES ("T7", "Alienware", now());
+INSERT INTO computadora (name, marca, created_at) VALUES ("A2", "Razer", now());
